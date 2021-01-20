c663d3bbb9418084645c54972c449fd4402d320e
shivasailesh <shivasailesh18@gmail.com>
2021-01-20 13:59:29 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home:Scaffold(
+    home:Home()
+  ));
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title:Text("My First app"),
         centerTitle: true,
+        backgroundColor:Colors.red[600],
       ),
       body:Center(
-        child: Text('hello welcome you all'),
+        child:IconButton(
+          onPressed: (){},
+          icon: Icon(Icons.alternate_email),
+          color:Colors.amber
+        ),
 
       ),
-        floatingActionButton:FloatingActionButton(
-          child: Text("click",
-          style:TextStyle(color: Colors.black)),
-        ) ,
-    ),
-  ));
+      floatingActionButton:FloatingActionButton(
+        onPressed: (){},
+        child: Text("click",
+            style:TextStyle(color: Colors.black)),
+        backgroundColor: Colors.red[600],
+      ) ,
+    );
+  }
+}
+
 
 
